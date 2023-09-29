66a80c119e2fb5f8c5ca484e0b4436e748a343c2
Joao Francisco Bratti <renamaciel45@gmail.com>
2023-09-29 09:02:45 -0300

    hhh

--- modulos/dividi.js
+++
@@ -1,4 +0,0 @@
-var dividi = function (x,y) {
-    return x /  y;
-  }; 
-  module.exports = dividi;
--- modulos/mensagem.js
+++
@@ -1,4 +0,0 @@
-var mensagem = function (nome,BOCA) {  
-    return "E o " + nome + BOCA;
-  }; 
-  module.exports = mensagem;
